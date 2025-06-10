a977c91842c64ca3bb092a52e96b40a6dc498d04
carsago <ohseo8813@gmail.com>
2025-06-10 20:58:51 +0900

    add Spring integration to HitterYearStatClient and its test

--- src/main/java/com/example/futures/infra/HitterYearStatClient.java
+++ src/main/java/com/example/futures/infra/HitterYearStatClient.java
@@ -1,10 +1,21 @@
 package com.example.futures.infra;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+
 import java.util.Collections;
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class HitterYearStatClient {
+
+    private final FuturesClient futuresClient;
+
     public List<HitterYearStat> getHitterYearStats(final int year) {
+        final ResponseEntity<String> response = futuresClient.getFuturesHitterStatPage(new LinkedMultiValueMap(), null);
         return Collections.emptyList();
     }
 }
--- src/test/java/com/example/futures/infra/HitterYearStatClientTest.java
+++ src/test/java/com/example/futures/infra/HitterYearStatClientTest.java
@@ -1,16 +1,21 @@
 package com.example.futures.infra;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SpringBootTest
 class HitterYearStatClientTest {
+
+    @Autowired
+    HitterYearStatClient sut;
 
      @Test
      void readYearStat() {
-         final var sut = new HitterYearStatClient();
          final var year = 2025;
 
          List<HitterYearStat> actual = sut.getHitterYearStats(year);
