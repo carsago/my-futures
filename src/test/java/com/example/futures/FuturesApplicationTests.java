ed1eb0d6cd499b32e9af744b2379272e1526261e
carsago <ohseo8813@gmail.com>
2025-06-10 20:26:41 +0900

    git init

---
+++ src/test/java/com/example/futures/FuturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.futures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
