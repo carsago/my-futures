4d5b18c492f69323d13fbff104c464385d64f2c9
carsago <ohseo8813@gmail.com>
2025-06-10 20:58:07 +0900

    fix API URL

--- src/main/java/com/example/futures/infra/FuturesClient.java
+++ src/main/java/com/example/futures/infra/FuturesClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.service.annotation.PostExchange;
 @HttpExchange
 interface FuturesClient {
 
-    @PostExchange("/Futures/Player/hitter.aspx")
+    @PostExchange("/futures/Player/hitter.aspx")
     ResponseEntity<String> getFuturesHitterStatPage(
             @RequestBody MultiValueMap<String, String> form,
             @RequestHeader(value = HttpHeaders.COOKIE, required = false) String cookie
